0024b90216554666d1fd4b91d78eb5ecc70b7c1b
gattlin1 <gattlin1@live.missouristate.edu>
2018-10-21 13:00:23 -0500

    updated comments, function readability and readme

--- hw/friend_circles/README.md
+++ hw/friend_circles/README.md
@@ -1,8 +1,6 @@
 Friend Circles Problem
 
-There are n
-
-students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature, i.e., if A is a friend of B and B is a friend of C, then A is also a friend of C.
+There are n students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature, i.e., if A is a friend of B and B is a friend of C, then A is also a friend of C.
 
 A friend circle is a group of students who are directly or indirectly friends. Indirect friendships can require multiple levels of indirection.
 
@@ -18,6 +16,4 @@ You can assume the following:
     Each element of input file will be Y or N
     The number of rows and columns in the input file will be equal
     All students are friends with themselves (the diagonal of the matrix is filled with Ys)
-    If studenti
-
-is friends with studentj, then studentj is friends with studenti
+    If studenti is friends with studentj, then studentj is friends with studenti
--- hw/friend_circles/src/Python/main.py
+++ hw/friend_circles/src/Python/main.py
@@ -8,21 +8,16 @@ instructor: Anthony Clark
 """
 @param1: list of the adjacency matrix for who the person is friends with
 @return: number of groups from the adjacency matrix
+Sets every person to unexplored. Then iterates through every person to 
+find who they are friends with (by calling dfs).
 """
 def count_friend_circles(friends):
-    #initializes the dictionary and sets the first value to unexplored
     friends_dict = {0: 'unexplored'}
-
-    #gives every person (node) the value 'unexplored'
-    for i in range(1, len(friends[0])): #start at 1 since we already set the value of 0.
-        friends_dict[i] = 'unexplored'
-
-    #initialized the groups value to 0. This will be used to return the number of groups in the end
     groups = 0
 
-    #iterates through all of the people in the friends dictionary
-    #if the person is unexplored then that is starting a new group
-    #because dfs will find the whole group when called.
+    for i in range(1, len(friends[0])): #start at 1 since we initialized with 0 value
+        friends_dict[i] = 'unexplored'
+
     for person in friends_dict:
         if friends_dict[person] == 'unexplored':
             groups += 1
@@ -31,24 +26,21 @@ def count_friend_circles(friends):
 
 """
 @param1: adjacency matrix list
-@param2: dictionary 
-@param3: start position
+@param2: dictionary for if the person (node) has beenn explored
+@param3: starting person (node)
 Searches the adjacency matrix to see if the starting person has any friends.
 If they do then explore that friend to see if they have any other friends.
 """
 def dfs(friends, friends_dict, start):
     friends_dict[start] = 'explored' #sets the person to explored
     for i in range(len(friends[start])):
-        #if they are friends and the person hasn't already been explored then check if that person
-        #has any more friends.
         if friends[start][i] == 'Y' and friends_dict[i] != 'explored':
             dfs(friends, friends_dict, i)
 
 if __name__ == '__main__':
-
     matrix_filename = input('Please provide a filename containing a friends matrix:\n')
 
     with open(matrix_filename, 'r') as matrix_file:
         friends = [line.strip() for line in matrix_file.readlines()]
     num_circles = count_friend_circles(friends)
-    print(f'Number of friend circles: {num_circles}')
+print(f'Number of friend circles: {num_circles}')
