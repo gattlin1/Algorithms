9ebacc49fb82335c50f07eb11d4f47b6543d38ba
gattlin1 <gattlin1@live.missouristate.edu>
2018-10-22 17:02:54 -0500

    changed the dictionary to an array since it better suits the problem

--- hw/friend_circles/src/Python/main.py
+++ hw/friend_circles/src/Python/main.py
@@ -8,34 +8,35 @@ instructor: Anthony Clark
 """
 @param1: list of the adjacency matrix for who the person is friends with
 @return: number of groups from the adjacency matrix
-Sets every person to unexplored. Then iterates through every person to 
+Sets every person to unexplored (false). Then iterates through every person to 
 find who they are friends with (by calling dfs).
 """
 def count_friend_circles(friends):
-    friends_dict = {0: 'unexplored'}
+    explored = [False]
     groups = 0
 
     for i in range(1, len(friends[0])): #start at 1 since we initialized with 0 value
-        friends_dict[i] = 'unexplored'
+        explored.append(False) #each index corresponds with one person.
 
-    for person in friends_dict:
-        if friends_dict[person] == 'unexplored':
+    for i in range(len(explored)):
+        if not explored[i]:
             groups += 1
-            dfs(friends, friends_dict, person)
+            dfs(friends, explored, i)
     return groups
 
 """
 @param1: adjacency matrix list
-@param2: dictionary for if the person (node) has beenn explored
-@param3: starting person (node)
+@param2: array of if the person has been explored or not. The index value 
+corresponds with the person. ex. index 0 represents person 1. etc...
+@param3: starting index
 Searches the adjacency matrix to see if the starting person has any friends.
 If they do then explore that friend to see if they have any other friends.
 """
-def dfs(friends, friends_dict, start):
-    friends_dict[start] = 'explored' #sets the person to explored
+def dfs(friends, explored, start):
+    explored[start] = True #sets the person to explored
     for i in range(len(friends[start])):
-        if friends[start][i] == 'Y' and friends_dict[i] != 'explored':
-            dfs(friends, friends_dict, i)
+        if friends[start][i] == 'Y' and not explored[i]:
+            dfs(friends, explored, i)
 
 if __name__ == '__main__':
     matrix_filename = input('Please provide a filename containing a friends matrix:\n')
